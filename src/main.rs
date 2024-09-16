44a9ed7c5f6f6eace46b1c7e1314c450e09e24b6
Cameron Baird <cameronbaird@microsoft.com>
2024-09-16 19:08:21 +0000

    Refactor command parsing; introduce start_vm as example command

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,69 @@
-use clap::{Parser, Subcommand};
+use clap::{Arg, ArgAction, ArgMatches, Command};
 
-/// Simple program to greet a person
-#[derive(Parser)]
-#[command(version, about, long_about = None)]
-struct Args {
-    #[command(subcommand)]
-    command: Option<Commands>,
-
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: Option<String>,
-
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+fn start_vm(cmd_arguments: &ArgMatches) {
+    // Start a VM
+    let name = cmd_arguments.get_one::<String>("name").unwrap();
+    println!("Starting VM: {}", name);
 }
 
-#[derive(Subcommand)]
-enum Commands {
-    /// does testing things
-    Test {
-        /// lists test values
-        #[arg(short, long)]
-        list: bool,
-    },
-}
+fn create_app() -> Command {
+    let app = Command::new("abathur")
+        .version("0.1.0")
+        .about("Abathur: a simple VM orchestrator")
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("start")
+                .about("Start a VM")
+                .arg_required_else_help(true)
+                .arg(
+                    Arg::new("name")
+                        .short('n')
+                        .long("name")
+                        .help("Name of the VM to start")
+                        .required(true),
+                )
+                .arg(
+                    Arg::new("kernel")
+                        .short('k')
+                        .long("kernel")
+                        .help("Kernel (or firmware) image to boot the VM")
+                        .required(true),
+                )
+                .arg(
+                    Arg::new("disk")
+                        .short('d')
+                        .long("disk")
+                        .help("disk image to add to the VM")
+                        .action(ArgAction::Append)
+                        .required(true),
+                )
+                .arg(
+                    Arg::new("cpus")
+                        .long("cpus")
+                        .help("number of cpus to allocate to the VM")
+                        .default_value("2")
+                )
+                .arg(
+                    Arg::new("memory")
+                        .long("memory")
+                        .help("amount of memory to allocate to the VM")
+                        .default_value("2048M")
+                )
+        );
 
+    app
+}
 
 fn main() {
-    let args = Args::parse();
+    let mut cmd = create_app();
+    let args = cmd.clone().get_matches();
 
-    for _ in 0..args.count {
-        match args.name {
-            None => println!("Hello!"),
-            Some(ref name) => println!("Hello {}!", name)
+    match args.subcommand() {
+        Some(("start", start_args)) => {
+            start_vm(start_args);
+        }
+        _ => {
+            let _ = cmd.print_help();
         }
     }
-
-    if let Some(command) = args.command {
-        match command {
-            Commands::Test { list } => {
-                if list {
-                    println!("Listing test values");
-                } else {
-                    println!("Running tests");
-                }
-            }
-        }
-    }
-
 }
