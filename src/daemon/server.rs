605d1bf5e600a305e99ad7c251de6cccd67db163
Cameron Baird <cameronbaird@microsoft.com>
2024-09-19 17:12:28 +0000

    Implement VM list

--- src/daemon/server.rs
+++ src/daemon/server.rs
@@ -60,6 +60,14 @@ async fn start_vm(vm_args: abathur::StartVm, vm_handles: Arc<Mutex<HashMap<Strin
     };
 }
 
+async fn list_vm(vm_handles: Arc<Mutex<HashMap<String, abathur::VmContext>>>) -> Result<impl warp::Reply, warp::Rejection> {
+    println!("Listing VMs");
+
+    let handles = vm_handles.lock().unwrap();
+    let handles: Vec<abathur::VmHandle> = handles.values().map(|c| c.handle.clone()).collect();
+    return Ok(warp::reply::json(&handles));
+}
+
 pub async fn server_main(cmd_arguments: &ArgMatches) {
     let port = cmd_arguments.get_one::<String>("port").unwrap();
     let port: u16 = port.parse().expect("Port must be a number");
@@ -69,35 +77,20 @@ pub async fn server_main(cmd_arguments: &ArgMatches) {
     let vm_handles = Arc::new(Mutex::new(HashMap::new()));
     let vm_handles_filter = warp::any().map(move || Arc::clone(&vm_handles));
 
-    // let start_vm = warp::path("start_vm")
-    //     .and(warp::post())
-    //     .and(warp::body::json())
-    //     .map(|vm: abathur::StartVm| {
-    //         start_vm(&vm);
-    //         warp::reply::json(&vm)
-    //     });
-
-    let start_vm = warp::post()
+    let start_vm_endpoint = warp::post()
         .and(warp::path("start_vm"))
         .and(warp::path::end())
         .and(warp::body::json())
         .and(vm_handles_filter.clone())
         .and_then(start_vm);
 
-    let list_vm = warp::path("list_vm")
-        .and(warp::get())
-        .map(|| {
-            println!("Listing VMs");
-            warp::reply::json(&vec![abathur::StartVm {
-                name: "test".to_string(),
-                kernel: "test".to_string(),
-                disks: vec!["test".to_string()],
-                cpus: "test".to_string(),
-                memory: "test".to_string(),
-            }])
-        });
+    let list_vm_endpoint = warp::get()
+        .and(warp::path("list_vm"))
+        .and(warp::path::end())
+        .and(vm_handles_filter.clone())
+        .and_then(list_vm);
 
-    let routes = start_vm.or(list_vm);
+    let routes = start_vm_endpoint.or(list_vm_endpoint);
 
     warp::serve(routes).run(([127, 0, 0, 1], port)).await;
 }
--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,27 @@ async fn send_vm_to_server(vm: abathur::StartVm) {
     }
 }
 
+async fn list_vms() {
+    let client = Client::new();
+    let res = client.get(format!("http://{}:{}/list_vm", DEFAULT_ADDRESS, DEFAULT_PORT))
+        .send()
+        .await;
+
+    match res {
+        Ok(response) => {
+            if response.status().is_success() {
+                let body = response.text().await.unwrap();
+                println!("{}", body);
+            } else {
+                eprintln!("Failed to list VMs: {}", response.status());
+            }
+        }
+        Err(e) => {
+            eprintln!("Error sending request: {}", e);
+        }
+    }
+}
+
 fn start_vm(cmd_arguments: &ArgMatches) {
     // Start a VM
     let name = cmd_arguments.get_one::<String>("name").unwrap();
@@ -63,42 +84,51 @@ fn create_app() -> Command {
         .about("Abathur: a simple VM orchestrator")
         .arg_required_else_help(true)
         .subcommand(
-            Command::new("start")
-                .about("Start a VM")
+            Command::new("vm")
+                .about("VM management commands")
                 .arg_required_else_help(true)
-                .arg(
-                    Arg::new("name")
-                        .short('n')
-                        .long("name")
-                        .help("Name of the VM to start")
-                        .required(true),
+                .subcommand(
+                    Command::new("list")
+                        .about("List all VMs")
                 )
-                .arg(
-                    Arg::new("kernel")
-                        .short('k')
-                        .long("kernel")
-                        .help("Kernel (or firmware) image to boot the VM")
-                        .required(true),
-                )
-                .arg(
-                    Arg::new("disk")
-                        .short('d')
-                        .long("disk")
-                        .help("disk image to add to the VM")
-                        .action(ArgAction::Append)
-                        .required(true),
-                )
-                .arg(
-                    Arg::new("cpus")
-                        .long("cpus")
-                        .help("number of cpus to allocate to the VM")
-                        .default_value("2")
-                )
-                .arg(
-                    Arg::new("memory")
-                        .long("memory")
-                        .help("amount of memory to allocate to the VM")
-                        .default_value("2048M")
+                .subcommand(
+                    Command::new("start")
+                        .about("Start a VM")
+                        .arg_required_else_help(true)
+                        .arg(
+                            Arg::new("name")
+                                .short('n')
+                                .long("name")
+                                .help("Name of the VM to start")
+                                .required(true),
+                        )
+                        .arg(
+                            Arg::new("kernel")
+                                .short('k')
+                                .long("kernel")
+                                .help("Kernel (or firmware) image to boot the VM")
+                                .required(true),
+                        )
+                        .arg(
+                            Arg::new("disk")
+                                .short('d')
+                                .long("disk")
+                                .help("disk image to add to the VM")
+                                .action(ArgAction::Append)
+                                .required(true),
+                        )
+                        .arg(
+                            Arg::new("cpus")
+                                .long("cpus")
+                                .help("number of cpus to allocate to the VM")
+                                .default_value("2")
+                        )
+                        .arg(
+                            Arg::new("memory")
+                                .long("memory")
+                                .help("amount of memory to allocate to the VM")
+                                .default_value("2048M")
+                        )
                 )
         )
         .subcommand(
@@ -125,8 +155,21 @@ fn main() {
     let args = cmd.clone().get_matches();
 
     match args.subcommand() {
-        Some(("start", start_args)) => {
-            start_vm(start_args);
+        Some(("vm", vm_args)) => {
+            match vm_args.subcommand() {
+                Some(("list", _)) => {
+                    let rt = tokio::runtime::Runtime::new().unwrap();
+                    rt.block_on(async {
+                        list_vms().await;
+                    });
+                }
+                Some(("start", start_args)) => {
+                    start_vm(start_args);
+                }
+                _ => {
+                    let _ = cmd.print_help();
+                }
+            }
         }
         Some(("daemon", daemon_args)) => {
             match daemon_args.subcommand() {
